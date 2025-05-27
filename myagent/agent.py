0ba3090cd8ae8d1db043cedf77f045554514d1d6
vishal-pandey <contact@vishalpandey.co.in>
2025-05-27 14:40:55 +0530

    Agent update

--- myagent/agent.py
+++ myagent/agent.py
@@ -8,7 +8,7 @@ from google.adk.tools import FunctionTool, ToolContext
 from pprint import pprint
 import httpx
 
-async def ask_human(tool_context: ToolContext, question: str) -> str:
+async def ask_underwriter(tool_context: ToolContext, question: str) -> str:
     # Extract session info
     session_id = tool_context._invocation_context.session.id
     user_id = tool_context._invocation_context.user_id
@@ -41,12 +41,10 @@ root_agent = Agent(
     name="my_agent",
     model="gemini-2.0-flash",
     description="""
-        You are a helpful agent you ask the human whatever question user asks you to ask the human.
-        You have a tool named ask_human that you can use to ask the human a question.
-        This is a human in the loop tool which will send a question to the human and wait for their response.
+        Yuor name is leo and you are a junior underwriter ai agent, given the initial analysis your job is to generate a initial report and present to the uer and also you need to ask for review from underwriter for approval of your decision. you need to show him your analysis in best possible so that he can provide the right feedback view to get the feedback from him in terms of approval, so while using the tool ask_underwriter, provide the analysis you have done so far and ask for his feedback in best possible way.
     """,
     tools=[
-        LongRunningFunctionTool(ask_human)
+        LongRunningFunctionTool(ask_underwriter)
         # MCPToolset(
         #     connection_params=SseServerParams(url="http://localhost:8900/mcp", headers={'Session-Id': example_session.id}),
         # )
---
+++ question.md
@@ -0,0 +1,2 @@
+This is AI analysis of underwriting of the case done by you lets say your name is leo. now you need to ask for review from underwriter(human) for approval of your decision. you need to show him your analysis in best possible so that he can provide the right feedback view to get the feedback from him in terms of approval
+
